64484b243b8cf06851035528392db0a12a9317ca
Taehyun Hwang <taehyun95@gmail.com>
2017-05-18 21:29:10 +0900

    [ProjectSetting] Add Unity Base Code

---
+++ Assets/02. Scripts/FollowCam.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour {
+	public Transform targetTr;
+	public float dist = 10.0f;
+	public float height = 3.0f;
+	public float dampTrace = 20.0f;
+	private Transform tr;
+
+	void Start () {
+		tr = GetComponent<Transform>();
+	}
+	
+	void LateUpdate () {
+		tr.position = Vector3.Lerp(tr.position
+									, targetTr.position
+									- (targetTr.forward * dist)
+									+ (Vector3.up * height)
+									, Time.deltaTime * dampTrace);
+		tr.LookAt(targetTr.position);
+	}
+}
---
+++ Assets/02. Scripts/PlayerCtrl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour {
+	private float h = 0.0f;
+	private float v = 0.0f;
+
+	private Transform tr;
+	public float moveSpeed = 10.0f;
+	public float rotSpeed = 100.0f;
+
+	// Use this for initialization
+	void Start () {
+		tr = GetComponent <Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		h = Input.GetAxis ("Horizontal");
+		v = Input.GetAxis ("Vertical");
+
+		Debug.Log ("H=" + h.ToString());
+		Debug.Log ("V=" + v.ToString());
+
+		Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+
+		tr.Translate(moveDir.normalized * Time.deltaTime * moveSpeed, Space.Self);
+
+		tr.Rotate(Vector3.up * Time.deltaTime * rotSpeed * Input.GetAxis("Mouse X"));
+	}
+}
