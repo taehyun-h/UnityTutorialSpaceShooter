9893ac30588eec124dfd2ae26b9c7e722e3cda86
Taehyun Hwang <taehyun95@gmail.com>
2017-06-08 19:28:45 +0900

    [Tutorial] Create Projectile And Effect - p.163 ~ p.182

---
+++ Assets/02. Scripts/WallCtrl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCtrl : MonoBehaviour
+{
+	void OnCollisionEnter(Collision coll)
+	{
+		if (coll.collider.tag == "BULLET")
+		{
+			Destroy(coll.gameObject);
+		}
+	}
+}
