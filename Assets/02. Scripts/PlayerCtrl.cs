7f7a89b578fadc380c6ea1c1d57b3aa896545a6c
Taehyun Hwang <taehyun95@gmail.com>
2017-06-08 17:50:09 +0900

    [Tutorial] Create Hero Character - p.112 ~ p.161

--- Assets/02. Scripts/PlayerCtrl.cs
+++ Assets/02. Scripts/PlayerCtrl.cs
@@ -2,6 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Anim
+{
+	public AnimationClip idle;
+	public AnimationClip runForward;
+	public AnimationClip runBackward;
+	public AnimationClip runRight;
+	public AnimationClip runLeft;
+}
+
 public class PlayerCtrl : MonoBehaviour {
 	private float h = 0.0f;
 	private float v = 0.0f;
@@ -10,9 +20,17 @@ public class PlayerCtrl : MonoBehaviour {
 	public float moveSpeed = 10.0f;
 	public float rotSpeed = 100.0f;
 
+	public Anim anim;
+	public Animation _animation;
+
 	// Use this for initialization
 	void Start () {
 		tr = GetComponent <Transform>();
+
+		_animation = GetComponentInChildren<Animation>();
+
+		_animation.clip = anim.idle;
+		_animation.Play();
 	}
 	
 	// Update is called once per frame
@@ -28,5 +46,26 @@ public class PlayerCtrl : MonoBehaviour {
 		tr.Translate(moveDir.normalized * Time.deltaTime * moveSpeed, Space.Self);
 
 		tr.Rotate(Vector3.up * Time.deltaTime * rotSpeed * Input.GetAxis("Mouse X"));
+
+		if (v >= 0.1f)
+		{
+			_animation.CrossFade(anim.runForward.name, 0.3f);
+		}
+		else if (v <= -0.1f)
+		{
+			_animation.CrossFade(anim.runBackward.name, 0.3f);
+		}
+		else if (h >= 0.1f)
+		{
+			_animation.CrossFade(anim.runRight.name, 0.3f);
+		}
+		else if (h <= -0.1f)
+		{
+			_animation.CrossFade(anim.runLeft.name, 0.3f);
+		}
+		else
+		{
+			_animation.CrossFade(anim.idle.name, 0.3f);
+		}
 	}
 }
