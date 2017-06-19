9d21b3005c360e09d1d5e4194bcc82a82232f129
Taehyun Hwang <taehyun95@gmail.com>
2017-06-19 15:37:09 +0900

    [Tutorial] Create Projectile And Effect - p.182 ~ p.226

---
+++ Assets/02. Scripts/BarrelCtrl.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelCtrl : MonoBehaviour
+{
+	public GameObject expEffect;
+	public Texture[] textures;
+	private Transform tr;
+
+	private int hitCount = 0;
+
+	void Start()
+	{
+		tr = GetComponent<Transform>();
+
+		int idx = Random.Range(0, textures.Length);
+		GetComponentInChildren<MeshRenderer>().material.mainTexture = textures[idx];
+	}
+
+	void OnCollisionEnter(Collision coll)
+	{
+		if (coll.collider.tag == "BULLET")
+		{
+			Destroy(coll.gameObject);
+			if (++ hitCount >= 3)
+			{
+				ExpBarrel();
+			}
+		}
+	}
+
+	void ExpBarrel()
+	{
+		Instantiate(expEffect, tr.position, Quaternion.identity);
+		Collider[] colls = Physics.OverlapSphere(tr.position, 10.0f);
+
+		foreach (Collider coll in colls)
+		{
+			Rigidbody rbody = coll.GetComponent<Rigidbody>();
+			if (rbody != null)
+			{
+				rbody.mass = 1.0f;
+				rbody.AddExplosionForce(1000.0f, tr.position, 10.0f, 300.0f);
+			}
+		}
+
+		Destroy(gameObject, 5.0f);
+	}
+}
--- Assets/02. Scripts/WallCtrl.cs
+++ Assets/02. Scripts/WallCtrl.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class WallCtrl : MonoBehaviour
 {
+	public GameObject sparkEffect;
+
 	void OnCollisionEnter(Collision coll)
 	{
 		if (coll.collider.tag == "BULLET")
 		{
+			GameObject spark = Instantiate(sparkEffect, coll.transform.position, Quaternion.identity);
+			Destroy(spark, spark.GetComponent<ParticleSystem>().main.duration + 0.2f);
 			Destroy(coll.gameObject);
 		}
 	}
